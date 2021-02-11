b232d1e179e0c717c233b1889d504eeeb6893dae
Romain Deneau <rdeneau@yahoo.fr>
2021-02-11 16:22:48 +0100

    reject invalid coordinates

--- ChessKata/Chess.fs
+++ ChessKata/Chess.fs
@@ -1,8 +1,23 @@
 module Chess
 
+open FSharpPlus
+open Helpers
+
 type Game = Game
 
 type Coordinate = string
 
-let move (piece: Coordinate) (to_square: Coordinate) (game: Game) : Result<Game, string> =
-  failwith "TODO"
+module Coordinate =
+  let parse (coordinate: Coordinate) =
+    match coordinate with
+    | Regex @"([a-h])([1-8])" [ col; row ] -> Ok [ col; row ]
+    | _ -> Error "invalid coordinate"
+
+let move (piece: Coordinate) (square: Coordinate) (game: Game) : Result<Game, string> =
+  monad' {
+    let! _piece  = Coordinate.parse piece  |> Result.mapError (fun x -> $"{x} ({nameof piece})")
+    and! _square = Coordinate.parse square |> Result.mapError (fun x -> $"{x} ({nameof square})")
+    return game
+    }
+
+let tmp = Game |> move "a1" "xx"
--- ChessKata/ChessShould.fs
+++ ChessKata/ChessShould.fs
@@ -1,11 +1,17 @@
 module ChessShould
 
 open Chess
-open FsUnit.Xunit
+open Swensen.Unquote
 open Xunit
+
+let emptyGame = Game
+
+[<Fact>]
+let ``Reject invalid piece coordinate`` () =
+  let result = emptyGame |> move "xx" "a1"
+  result =! Error "invalid coordinate (piece)"
 
 [<Fact>]
 let ``Reject invalid square coordinate`` () =
-    Game
-    |> move "a1" "xx"
-    |> should equal (Error "invalid square coordinate")
+  let result = emptyGame |> move "a1" "xx"
+  result =! Error "invalid coordinate (square)"
