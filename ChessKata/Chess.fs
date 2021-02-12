fca2dd3f5bc2f7e202f7999bb6abdf1a0493061e
Romain Deneau <rdeneau@yahoo.fr>
2021-02-12 12:49:59 +0100

    test Move piece to the given coordinate

--- ChessKata/Chess.fs
+++ ChessKata/Chess.fs
@@ -2,22 +2,55 @@ module Chess
 
 open FSharpPlus
 open Helpers
+open System
 
-type Game = Game
+type File = a = 1 | b = 2 | c = 3 | d = 4 | e = 5 | f = 6 | g = 7 | h = 8 // Column
+type Rank = _1 = 1 | _2 = 2 | _3 = 3 | _4 = 4 | _5 = 5 | _6 = 6 | _7 = 7 | _8 = 8 // Row
+type SquareNotation = string // E.g. "a1"
+type Square = { Notation: SquareNotation; File: File; Rank: Rank }
+type Color = Black | White
+type Piece = King | Queen | Rook | Bishop | Knight | Pawn
+type PieceSymbol = string
+type Game = { Board: Map<Square, Color * Piece>; Turn: Color }
 
-type Coordinate = string
+module Piece =
+  let parse (piece: PieceSymbol) =
+    match piece with
+    | "♔" -> White, King
+    | "♕" -> White, Queen
+    | "♖" -> White, Rook
+    | "♗" -> White, Bishop
+    | "♘" -> White, Knight
+    | "♙" -> White, Pawn
+    | "♚" -> Black, King
+    | "♛" -> Black, Queen
+    | "♜" -> Black, Rook
+    | "♝" -> Black, Bishop
+    | "♞" -> Black, Knight
+    | "♟︎" -> Black, Pawn
+    | s -> failwith $"invalid piece {s}"
 
-module Coordinate =
-  let parse (coordinate: Coordinate) =
-    match coordinate with
-    | Regex @"([a-h])([1-8])" [ col; row ] -> Ok [ col; row ]
-    | _ -> Error "invalid coordinate"
+module Square =
+  let parse (notation: SquareNotation) =
+    match notation with
+    | Regex @"([a-h])([1-8])" [ file; rank ] ->
+      {
+        Notation = notation
+        File     = Enum.Parse(typeof<File>, file) :?> File
+        Rank     = rank |> int |> enum<Rank>
+      }
+    | _ -> failwith "invalid coordinate"
 
-let move (piece: Coordinate) (square: Coordinate) (game: Game) : Result<Game, string> =
-  monad' {
-    let! _piece  = Coordinate.parse piece  |> Result.mapError (fun x -> $"{x} ({nameof piece})")
-    and! _square = Coordinate.parse square |> Result.mapError (fun x -> $"{x} ({nameof square})")
-    return game
-    }
+let add (piece: PieceSymbol) (square: SquareNotation) (game: Game) : Game =
+  let board = game.Board |> Map.add (square |> Square.parse) (piece |> Piece.parse)
+  { game with Board = board }
 
-let tmp = Game |> move "a1" "xx"
+let move (piece: SquareNotation) (target: SquareNotation) (game: Game) : Result<Game, string> =
+  let pieceSquare  = Square.parse piece
+  let targetSquare = Square.parse target
+  if pieceSquare = targetSquare then
+    Error "no move"
+  elif true then
+    Error $"no piece at {piece}"
+  else
+    Ok game
--- ChessKata/ChessShould.fs
+++ ChessKata/ChessShould.fs
@@ -1,17 +1,35 @@
 module ChessShould
 
 open Chess
+open FsUnit
 open Swensen.Unquote
 open Xunit
 
-let emptyGame = Game
+let emptyGame () = { Board = Map.empty; Turn = White }
 
 [<Fact>]
 let ``Reject invalid piece coordinate`` () =
-  let result = emptyGame |> move "xx" "a1"
-  result =! Error "invalid coordinate (piece)"
+  let act () = emptyGame () |> move "xx" "a1" |> ignore
+  act |> shouldFail
 
 [<Fact>]
-let ``Reject invalid square coordinate`` () =
-  let result = emptyGame |> move "a1" "xx"
-  result =! Error "invalid coordinate (square)"
+let ``Reject invalid target coordinate`` () =
+  let act () = emptyGame () |> move "a1" null |> ignore
+  act |> shouldFail
+
+[<Fact>]
+let ``Reject no move`` () =
+  let result = emptyGame () |> move "c3" "c3"
+  result =! Error "no move"
+
+[<Fact>]
+let ``Reject no piece at the given coordinate`` () =
+  let game = emptyGame () |> add "♙" "e2"
+  let result = game |> move "c3" "c4"
+  result =! Error "no piece at c3"
+
+[<Fact>]
+let ``Move piece to the given coordinate`` () =
+  let game = emptyGame () |> add "♙" "e2"
+  let result = game |> move "e2" "e4"
+  result =! Ok (emptyGame () |> add "♙" "e4")
